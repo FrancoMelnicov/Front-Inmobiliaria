203ca11c6030a5676b38e6aa13ebec36bba53259
Franco Melnicov <francogalaxy007@gmail.com>
2021-01-23 17:18:58 -0300

    arreglos de categoria y links

--- src/app/servicios/global.ts
+++ src/app/servicios/global.ts
@@ -1,6 +1,6 @@
 //creamos esta variable global para no tener que estar creandola varias veces
 //RECORDAR CAMBIARLA CUANDO SE SUBA AL SERVIDOR!!!!
 export var Global = {
-    url: 'http://localhost:3700/api/'
+    url: 'https://inmobiliaria-fangio.herokuapp.com/api/'
     //url2: http://localhost:3700/api/ https://inmobiliaria-fangio.herokuapp.com/api/
 }
